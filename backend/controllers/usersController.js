f6d3148a22f04dd0dccf8d67e66eeb4326c8317a
mjordanpascual <mjordanpascual@yahoo.com>
2026-04-03 00:46:11 +0800

    Add User Validations in Register Form in Backend

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -4,15 +4,56 @@ const db = require('../config/database');
 
 // Register user
 const registerUser = async (req, res) => {
-    const { username, password } = req.body; 
+    const { username,
+            password,
+            confirm_password,
+            email,
+            role,
+            department } = req.body; 
 
-    try {
+    try { 
+        // 1. Basic Validation - CHECK ALL FIELDS
+        if (!username || !password || !confirm_password || !email) {
+        return res.status(400).json({
+                message: 'All fields are required'
+        });
+        }
+
+        // 2. Basic Validation - USERNAME
+        if(username.length < 4){
+            return res.status(400).json({
+                message: 'Username must be at least 4 characters'
+        });
+        }
+        // 3. Basic Validation - PASSWORD
+        if (password.length < 6) {
+        return res.status(400).json({
+            message: 'Password must be at least 6 characters'
+        });
+        }
+
+        // 4. Basic Validation - Password Match Validation
+        if(password !== confirm_password){
+            return res.status(400).json({
+                message: 'Password and Confirm Password do not match'
+            });
+        }
+
+        // ✅ 5. Check if USERNAME OR PASSWORD already exists
+        const checkSql = `SELECT * FROM web_useraccount WHERE username = ? OR email = ?`;
+        const [existingUser] = await db.execute(checkSql, [username, email]);
+        if (existingUser.length > 0) {
+            return res.status(400).json({
+                message: 'Username or email already exists'
+            });
+        }
+
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // Insert user into the database
-        const query = 'INSERT INTO web_useraccount ( username, password ) VALUES ( ?, ?)';
-        await db.execute(query, [username, hashedPassword]);
+        const query = 'INSERT INTO web_useraccount ( username, password, email ) VALUES ( ?, ?, ?)';
+        await db.execute(query, [username, hashedPassword, email]);
 
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
